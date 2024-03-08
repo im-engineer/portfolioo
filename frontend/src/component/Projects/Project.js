fd9baa36e0f620343d18c14249b138eaf38007a7
siddhantsingh-sdn <siddhantsingh@smartdatainc.net>
2024-03-08 17:18:43 +0530

    changes in project component and navbar component

--- frontend/src/component/Projects/Project.js
+++ frontend/src/component/Projects/Project.js
@@ -3,64 +3,92 @@ import React from "react";
 import "./Project.css";
 
 import img1 from "../../Data/image/ECG.png";
-import img3 from "../../Data/image/pxalm.png";
 import img2 from "../../Data/image/OXVirtualCare.png";
+import img3 from "../../Data/image/pxalm.png";
+import img4 from "../../Data/image/DrivingSafety.png";
 
 function Project() {
   return (
     <>
-      <header className="header" style={{ marginBottom: "3rem" }}>
-        <div className="header-content">
-          {" "}
+      <div className="section">
+        <div className="line-styling">
+          <div className="style-circle"></div>
+          <div className="style-circle"></div>
+          <div className="style-line"></div>
+        </div>
+        <div className="header">
           <h3>Something I've Built</h3>
         </div>
-      </header>
-
-      <div className="projects" id="projects">
-        <div className="project-content">
-          <div className="project--img">
-            <div className="image-container">
-              <img size="100" src={img1} alt="" className="project--img" />
+        <div className="divvv">
+          <div className="body--col--1">
+            <div className="data1">
+              <div className="project---img">
+                <img src={img1} alt="" className="image1" />
+              </div>
             </div>
 
-            <p className="project--desc--p2">
-              The goal of a Pxalm is to accelerate the personal and professional
-              development of mentees. This is achieved by providing mentees with
-              guidance, advice and feedback from mentors with more experience
-              than themselves.
-            </p>
+            <div className="data1">
+              <div className="project---img">
+                <img src={img2} alt="" className="image1" />
+              </div>
+            </div>
 
-            <div className="image-container">
-              <img
-                size="100"
-                src={img3}
-                alt=""
-                className="project--img"
-                style={{ marginTop: "4rem", marginBottom: "4rem" }}
-              />
+            <div className="data1">
+              <div className="project---img">
+                <img src={img3} alt="" className="image1" />
+              </div>
+            </div>
+
+            <div className="data1">
+              <div className="project---img">
+                <img src={img4} alt="" className="image1" />
+              </div>
             </div>
           </div>
-        </div>
-
-        <div className="project-content2">
-          <div className="project--desc">
-            <p className="project--desc--p1">
-              The goal of a Pxalm is to accelerate the personal and professional
-              development of mentees. This is achieved by providing mentees with
-              guidance, advice and feedback from mentors with more experience
-              than themselves.
-            </p>
-
-            <div className="project--img2">
-              <img size="100" src={img2} alt="" className="project--img2" />
+          <div className="body--col--2">
+            <div className="data2">
+              <div className="project---desc">
+                <p className="project--desc--p1">
+                  The goal of a Pxalm is to accelerate the personal and
+                  professional development of mentees. This is achieved by
+                  providing mentees with guidance, advice and feedback from
+                  mentors with more experience than themselves.
+                </p>
+              </div>
             </div>
 
-            <p className="project--desc--p3">
-              The goal of a Pxalm is to accelerate the personal and professional
-              development of mentees. This is achieved by providing mentees with
-              guidance, advice and feedback from mentors with more experience
-              than themselves.
-            </p>
+            <div className="data2">
+              <div className="project---desc">
+                <p className="project--desc--p1">
+                  The goal of a Pxalm is to accelerate the personal and
+                  professional development of mentees. This is achieved by
+                  providing mentees with guidance, advice and feedback from
+                  mentors with more experience than themselves.
+                </p>
+              </div>
+            </div>
+
+            <div className="data2">
+              <div className="project---desc">
+                <p className="project--desc--p1">
+                  The goal of a Pxalm is to accelerate the personal and
+                  professional development of mentees. This is achieved by
+                  providing mentees with guidance, advice and feedback from
+                  mentors with more experience than themselves.
+                </p>
+              </div>
+            </div>
+
+            <div className="data2">
+              <div className="project---desc">
+                <p className="project--desc--p1">
+                  The goal of a Pxalm is to accelerate the personal and
+                  professional development of mentees. This is achieved by
+                  providing mentees with guidance, advice and feedback from
+                  mentors with more experience than themselves.
+                </p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
