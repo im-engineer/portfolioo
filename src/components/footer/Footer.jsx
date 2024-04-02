a01a31bc8fb5c3620d40548c4b680bcc26784ce6
Siddhant Singh <119558294+im-engineer@users.noreply.github.com>
2024-04-03 02:23:11 +0530

    third changes

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -22,9 +22,7 @@ const Footer = () => {
         <li>
           <a href="#portfolio">Portfolio</a>
         </li>
-        {/* <li>
-          <a href="#testimonials">Testimonials</a>
-        </li> */}
+
         <li>
           <a href="#contact">Contact</a>
         </li>
